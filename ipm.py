23a1225c93bf928e413de4072d32b0aae0d56b1c
fibbanachi <49879947+Programmeris@users.noreply.github.com>
2023-09-24 16:06:42 +0300

    check ip list file existing

--- ipm.py
+++ ipm.py
@@ -12,6 +12,10 @@ args = parser.parse_args()
 send_url = 'https://api.telegram.org/bot{token}/sendMessage'.format(token=args.bot_token)
 not_available_ip_list = ""
 
+if not os.path.exists(args.file):
+        print('{file} must be exist. Please fix it and try again'.format(file=args.file))
+        quit()
+
 with open(args.file) as file:
     park = file.read()
     park = park.splitlines()
